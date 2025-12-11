7c77b3bc52dc7031a2df9c76a57f86143c177d86
kshitij katore <kshitijkatore03@gmail.com>
2025-12-11 21:49:44 +0530

    To-Do using DOM

--- Week_4/01_Todo_List/script.js
+++ Week_4/01_Todo_List/script.js
@@ -1,4 +1,4 @@
-// fetching element
+/*// fetching element
 addTodo=()=>{
     const inputElement = document.querySelector("input");
     const inputValue = inputElement.value;
@@ -11,6 +11,49 @@ deleteTodo=(index)=>{
     const element = document.getElementById("todo-"+ index);
     element.parentNode.removeChild(element);
 }
-
-
 // Adding element
+*/
+
+
+
+const main = document.createElement("div");
+main.setAttribute('class', 'main');
+main.innerHTML = "Cohort - To-Do Application"
+
+const divMain = document.createElement("div");
+divMain.setAttribute('class','head');
+
+// // add input and button on head-class
+
+const inputElement = document.createElement("input");
+inputElement.placeholder = "Enter your task"
+const buttonElement = document.createElement("button");
+buttonElement.setAttribute('onclick','addTodo()')
+buttonElement.innerHTML = "Add";
+
+divMain.appendChild(inputElement);
+divMain.appendChild(buttonElement);
+
+// function add-todo
+
+function addTodo(){
+    const value = document.querySelector("input").value;
+    const h4Element = document.createElement("h4");
+    const buttonElement = document.createElement("button");
+    buttonElement.setAttribute("onclick", "deleteTodoAt(" + index + ")");
+    buttonElement.innerHTML = "Delete";
+
+    const divElement = document.createElement("div");
+    divElement.setAttribute('class','task')
+    divElement.appendChild(h4Element);
+    divElement.appendChild(buttonElement);
+
+    h4Element.innerHTML = value;
+    document.querySelector(".main").appendChild(divElement);
+}
+
+main.appendChild(divMain);
+document.body.appendChild(main);
+
+
+
